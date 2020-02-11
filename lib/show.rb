e87098839de8bf4f53884ff02b2d86db8e3cc72c
Maxwell Baird <bairdm71@gmail.com>
2020-02-11 09:52:47 -0700

    added the method actors

--- lib/show.rb
+++ lib/show.rb
@@ -18,4 +18,9 @@ class Show
     highest_salary = @characters.max_by {|actor| actor.salary}
     highest_salary.actor
   end
+
+  def actors
+    list_of_actors = @characters.sort_by{|actor| actor.actor}
+    actors_names = list_of_actors.map{|actor_name|actor_name.actor}
+  end
 end
