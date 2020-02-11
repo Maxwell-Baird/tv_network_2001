d04a17b2c812274907a00f28bcf9f2e1987a5bec
Maxwell Baird <bairdm71@gmail.com>
2020-02-11 09:47:52 -0700

    added highest_paid_actor method to class show

--- lib/show.rb
+++ lib/show.rb
@@ -11,6 +11,11 @@ class Show
   end
 
   def total_salary
-    total_salary = @characters.sum {|actor|actor.salary}
+    total_salary = @characters.sum {|actor| actor.salary}
+  end
+
+  def highest_paid_actor
+    highest_salary = @characters.max_by {|actor| actor.salary}
+    highest_salary.actor
   end
 end
