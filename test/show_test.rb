01dd62ada05ded37a6fa2532293ec75f3d50d394
Maxwell Baird <bairdm71@gmail.com>
2020-02-11 09:33:28 -0700

    added a test to check the class attributes

--- test/show_test.rb
+++ test/show_test.rb
@@ -4,10 +4,20 @@ require './lib/character'
 require './lib/show'
 
 class ShowTest < Minitest::Test
+
   def test_show_exist
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
     knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
     assert_instance_of Show, knight_rider
+  end
+
+  def test_show_attributes
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    assert_equal 'Knight Rider', knight_rider.name
+    assert_equal "Glen Larson", knight_rider.creator
+    assert_equal [kitt, michael_knight], knight_rider.characters
   end
 end
